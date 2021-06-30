55ca156d30e84178373761f359784150f407875b
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-06-30 22:46:52 +0600

    SearchBar component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import react from "react";
 import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
 const App = () => {
   return (
     <div>
       <Navbar />
+      <SearchBar />
     </div>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const SearchBar = () => {
-    return (
-        <div>
-        
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <section className="col s6 offset-s4">
+          <form action="" onSubmit="">
+            <div className="input-field">
+              <input placeholder="search for movies" type="text" onChange="" />
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
